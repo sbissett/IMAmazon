3457abe774ed3e85def442ca63ffdd1de9e396b1
Stephen Bissett <steve.bissett5@gmail.com>
2018-11-11 12:43:24 -0700

    addedsqlseeds

---
+++ sqlSeeds.sql
@@ -0,0 +1,16 @@
+USE bamazon;
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Amazon Echo", "Electronics", "179", "100"),
+("Fire HD 8 Tablets", "Technology", "80", "300"),
+("Sony Wireless Headphones", "Electronics", "148", "150"),
+("iRobot Roomba 652 Vacuum Cleaner", "Electronics", "375", "100"),
+("Kindle Paperwhite", "Technology", "190", "200"),
+("Fire TV Stick", "Technology", "40", "100"),
+("Monopoly", "Games", "20", "30"),
+("Keurig", "Kitchen", "80", "80"),
+("Yoga Mat", "Sports", "17", "50"),
+("Jenga", "Games", "10", "30");
+
+
