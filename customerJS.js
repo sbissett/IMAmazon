722f18dd1f9061a21ff6caf664d894c294240c25
Stephen Bissett <steve.bissett5@gmail.com>
2018-11-12 09:42:54 -0700

    withcustomerinput

--- customerJS.js
+++ customerJS.js
@@ -42,37 +42,60 @@ function displayAll() {
     }); //end connection to products
 };
 
-//Function to prompt customer to enter id of product to purchase
-
-function productID() {
-    inquirer.prompt ([{
-        name: 'produtID',
-        type: "input",
-        message: "Please enter the Id of the product you wish you buy"
+//Function to prompt user to enter id of product to buy
+function productId() {
+    inquirer.prompt([
+        {
+            name: 'productId',
+            type: "input",
+            message: "Please enter the Id of the product you wish to buy!"
         }
     ]).then(function(answer){
-        var selection = answer.productID;
-        connection.query("SELECT * FROM products WHERE id=?", selection,function(err,res){
+        var selection = answer.productId;
+        connection.query("SELECT * FROM Products WHERE id=?", selection, function(err, res){
             if (err) throw err;
 
-        //If results = 0 then prompt user to pick again and run productId function once more
-            if (res.length===0) {
-                console.log('That product is either out of stock or doesnt exit');
-                productID();
-            }    
-            else 
+            //If results equal to 0 then prompt user to pick again and run productId function again
+            if (res.length === 0){
+                console.log('That product does not exist, please enter an Id number from the list above');
+                productId();
+            }
+            //Else if id does exist, ask user how many items they would like to buy
+            else{
                 inquirer.prompt([
                     {
-                        name:'productQuantity',
+                        name: 'productQuantity',
                         type: "input",
-                        message: "How many items would you like to order today?"
-                    }    
+                        message: "How many items would you like to buy?!"
+                    }
+                ]).then(function(answer){
+                    var quantity = answer.productQuantity;
+                    //If quantity is less than stock quantity display error message
+                    if (quantity > res[0].stock_quantity) {
+                        console.log("Cannot proceed, we only have " + res[0].stock_quantity + " items of the product selected");
+                        productId();
+                    }
+                    //Else display the name and price of product chosen
+                    else {
+                        console.log("");
+                        console.log(res[0].product_name + ' purchased.');
+                        console.log(quantity + ' qty @ $' + res[0].price);
 
-                ])
-        })
-
-    })
-
-
-
-}0;
+                        //Sale total for product user chose
+                        var saleTotal = quantity * res[0].price;
+                        console.log("Sale Total: $"  + saleTotal);
+                        var newQuantity = res[0].stock_quantity - quantity;
+                        connection.query("UPDATE products SET stock_quantity = "+ newQuantity + " WHERE id = " + res[0].id , function (err, resUpdate){
+                            if (err) throw err;
+                            console.log("");
+                            console.log("Your Order has been Processed");
+                            console.log("Thank you for Shopping with us!");
+                        });
+                        //Ends connection
+                        connection.end();
+                    }//end of else for second question
+                });
+            };//end of else for first question
+        });//end of query
+    });
+};//end of inquirer
